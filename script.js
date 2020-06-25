9d4746204d4a1118bc756adb0b153e5ee02f35d5
mfd713 <friendmadeline@gmail.com>
2020-06-25 16:43:20 -0500

    initialized repository

---
+++ script.js
@@ -0,0 +1,38 @@
+let Fiona = {
+    "trait1": {
+        "phenotype" : "Slob",
+        "genotype" : "Aa"
+    },
+    "trait2": {
+        "phenotype" : "Music Lover",
+        "genotype" : "Aa"
+    },
+    "trait3" : {
+        "phenotype" : "Athletic",
+        "genotype" : "Aa"
+    },
+};
+
+let Shrek = {
+    "trait1": {
+        "phenotype" : "Slob",
+        "genotype" : "Aa"
+    },
+    "trait2": {
+        "phenotype" : "Loner",
+        "genotype" : "Aa"
+    },
+    "trait3" : {
+        "phenotype" : "Self-Assured",
+        "genotype" : "Aa"
+    },
+};
+
+let gamete1 = new Object();
+gamete1.name = "Slob";
+gamete1.allele = "Aa";
+
+let gamete2 = new Object();
+
+function makeSim(sim1, sim2){
+};
