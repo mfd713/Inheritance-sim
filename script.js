0c602a366f3b241591c493f766d8138a64552b98
mfd713 <friendmadeline@gmail.com>
2020-06-25 21:53:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 let Fiona = {
-    "trait1": {
-        "phenotype" : "Slob",
-        "genotype" : "Aa"
-    },
-    "trait2": {
-        "phenotype" : "Music Lover",
-        "genotype" : "Aa"
-    },
-    "trait3" : {
-        "phenotype" : "Athletic",
-        "genotype" : "Aa"
-    },
+    traitList: [{
+        phenotype : "Slob",
+        genotype : "Aa"
+    },{
+        phenotype : "Athletic",
+        genotype : "Aa"
+    },{
+        phenotype : "Music Lover",
+        genotype : "Aa"
+    }]
 };
 
-let Shrek = {
-    "trait1": {
-        "phenotype" : "Slob",
-        "genotype" : "Aa"
-    },
-    "trait2": {
-        "phenotype" : "Loner",
-        "genotype" : "Aa"
-    },
-    "trait3" : {
-        "phenotype" : "Self-Assured",
-        "genotype" : "Aa"
-    },
+let Shrek = { 
+    traitList: [{
+        phenotype : "Slob",
+        genotype : "Aa"
+    },{
+        phenotype : "Loner",
+        genotype : "Aa"
+    },{
+        phenotype : "Self-Assured",
+        genotype : "Aa"
+    }]
 };
+
+
 
 let gamete1 = new Object();
 let gamete2 = new Object();
@@ -36,8 +34,18 @@ let gamete5 = new Object();
 let gamete6 = new Object();
 let genePool = [gamete1, gamete2, gamete3, gamete4, gamete5, gamete6];
 
+
 function makeSim(sim1, sim2){
-    for(let i = 0; i++; i<6){
-        
-    }
+    for(let i = 0; i<3; i++){
+        for(let j = 0; j<3; j++){
+            if(sim1.traitList[i].phenotype === sim2.traitList[j].phenotype)
+               console.log("match");}
+            }
 };
+
+function getGamete(trait){
+    return {
+        name : trait.phenotype,
+        allele : trait.genotype[Math.round(Math.random())]
+    }
+}
